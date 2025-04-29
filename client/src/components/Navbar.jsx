4914d85baaf275e7e1d2f26c4e7990f91368a4bb
Hélène Olivares del Pino <hodelpino@MacBook-Air-de-Helene.local>
2025-04-29 02:04:52 -0400

    login  button and responsive navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,20 +1,36 @@
-import React from 'react'
-import '../style/Navbar.css'
+import React, { useState } from 'react';
+import '../style/Navbar.css';
+
 export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <nav className='navbar'>
-        <div className="navbar-logo">
-            <img src="/logo.png" alt="chambix logo" />
-        </div>
-        <ul className='navbar-links'>
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">Jobs</a></li>
-            <li><a href="/jobs">About</a></li>
-            <li><a href="/contact">Login</a></li>
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <img src="/logo.png" alt="chambix logo" />
+      </div>
+
+      {/* Links */}
+      <div className={`navbar-links-container ${isMenuOpen ? 'active' : ''}`}>
+        <ul className="navbar-links">
+          <li><a href="/">Home</a></li>
+          <li><a href="/jobs">Jobs</a></li>
+          <li><a href="/about">About</a></li>
         </ul>
+      </div>
+
+      {/* Botones */}
+      <div className="navbar-actions">
+        <a href="/login" className="login-button">Login</a>
         <div className="post-job">
-            <button>Post a Job</button>
+          <button>Post a Job</button>
         </div>
+        <div className="hamburger" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
     </nav>
   );
 }
