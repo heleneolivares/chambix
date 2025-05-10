a645483d86e2e9d712f5ed4e3fad0b7db82380c9
Hélène Olivares del Pino <hodelpino@MacBook-Air-de-Helene.local>
2025-05-09 23:50:55 -0400

    Login underconstruction

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,8 @@
+import '../style/Login.css';
 export default function Login() {
     return (
       <main className="main">
-        <h1>Login</h1>
-        <p>Contenido de la sección Login.</p>
+        <img className="login-image" src="https://images.unsplash.com/photo-1590479773265-7464e5d48118?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
       </main>
     );
   }
